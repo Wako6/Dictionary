d9d6e1f89986c5e04861ac9b3498483c867007d4
Wako6 <wakohd@gmail.com>
2020-10-02 15:59:41 +0200

    [UPDATE] Add functionnality that save unknow word

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,12 +1,14 @@
 from .storage import Table
 from tinydb import Query
 from .storagekit import _contains, _get_synonyms_on, _get_external_synonyms
+from variables import Variables
 
 
 class Dictionary:
 
     def __init__(self, dictionary_name='dictionary'):
         self.bdd = Table(dictionary_name)
+        self.unknow_words = list()
 
     def __contains__(self, str_word):
         Word = Query()
@@ -14,11 +16,21 @@ class Dictionary:
 
     def __getitem__(self, str_word):
         Word = Query()
-        return self.bdd.find(Word.label.test(_contains, str_word))
+        result = self.bdd._db.get(Word.label.test(_contains, str_word))
+
+        if result is None:
+            self.unknow_words.append(str_word)
+
+        return result
 
     def __iter__(self):
-        Word = Query()
-        return iter(self.bdd.find(Word))
+        return iter(self.bdd.all())
+
+    def __del__(self):
+        if len(self.unknow_words) > 0:
+            with open(Variables.UNKNOW_WORDS_PATH, 'w', encoding='utf-8', errors='ignore') as f:
+                for uw in self.unknow_words:
+                    f.write(uw)
 
     def lemmatize(self, sentence):
         pass
@@ -31,7 +43,6 @@ class Dictionary:
         external_synonyms = _get_external_synonyms(self.bdd, str_word)
 
         for s in external_synonyms:
-            # print(s)
             synonyms += _get_synonyms_on(self.bdd, s, str_word)
             synonyms += _get_synonyms_on(self.bdd, str_word, s)
 
