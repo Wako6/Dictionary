1a75913597a98cabf0d80d7124cf29e57ab518b7
Wako6 <wakohd@gmail.com>
2020-10-05 18:00:48 +0200

    [UPDATE] Fix a bug in variables class

--- variables.py
+++ variables.py
@@ -5,4 +5,5 @@ import os
 
 class Variables:
     DATABASE_PATH = 'datas'
-    PROJECT_PATH = 'C:\\PyWorkspace\\dictionary-env\\dictionary'  # os.getcwd()
+    PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+    UNKNOW_WORDS_PATH = 'unknow_word_list.txt'
